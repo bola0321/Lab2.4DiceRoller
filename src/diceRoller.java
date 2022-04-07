3a5e3a6b93de91b0ccbf8883f62126f049202a6b
Bolanle <bola0321@gmail.com>
2022-04-07 16:57:03 -0400

    added yesNo method and do-while loop

--- src/diceRoller.java
+++ src/diceRoller.java
@@ -11,37 +11,49 @@ public class diceRoller {
 		int numSides;
 		int randomRollOne;
 		int randomRollTwo;
-		int rollCounter;
+		int rollCounter = 0;
+		boolean cont = true;
 
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Welcome to the Grand Circus Casino!");
-		System.out.print("\n" + "How many sides should each die have?");
-		numSides = scnr.nextInt();
-
-		randomRollOne = generateRandomDice(numSides);
-		randomRollTwo = generateRandomDice(numSides);
-
-		System.out.println(randomRollOne + 1);
-		System.out.println(randomRollTwo + 1);
-
-		int dieSum = randomRollOne + randomRollTwo;
-
-		if (dieSum == 2 || dieSum == 3 || dieSum == 12) {
-			System.out.println("Craps!");
-		} else if (randomRollOne == 0 && randomRollTwo == 0) {
-			System.out.println("Snake Eyes!");
-		} else if (randomRollOne == 5 && randomRollTwo == 5) {
-			System.out.println("Boxcars!");
-		}
-
+		
+		do {
+			rollCounter+=1;
+			System.out.print("\n" + "How many sides should each die have?");
+			numSides = scnr.nextInt();
+	
+			randomRollOne = generateRandomDice(numSides);
+			randomRollTwo = generateRandomDice(numSides);
+	
+			System.out.println(randomRollOne + 1);
+			System.out.println(randomRollTwo + 1);
+	
+			int dieSum = randomRollOne + randomRollTwo;
+	
+			if (rollCounter == 1)
+				if(dieSum == 0 || dieSum == 1 || dieSum == 10) {
+				System.out.println("Craps!");
+			} else if (randomRollOne == 0 && randomRollTwo == 0) {
+				System.out.println("Snake Eyes!");
+			} else if (randomRollOne == 5 && randomRollTwo == 5) {
+				System.out.println("Boxcars!");
+			}
+			
+			cont = askUserYN("Would you like to roll again (y/n)? ");
+			System.out.println(cont);
+			
+		}while(cont == true);
+		
 // TODO: insert roll counter logic (roll 1, roll2, roll 3, etc....)
 		scnr.close();
 
 	}
 
-	/********************* generateRandomDice ********************** 
-	 * passes in int and randomly chooses number, returns an int
-	 */
+		/********************* generateRandomDice **********************
+		 * 
+		 * @param numSides
+		 * @return 
+		 */
 
 	public static int generateRandomDice(int numSides) {
 
@@ -49,12 +61,25 @@ public class diceRoller {
 
 		return randomRoll;
 
-//	}
+	}
 
-//****************** generateRandomDieRoll **************	
-		// public static void generateRandomDieRoll() {
-
-		// }
+		/************************* askUserYN ****************************	
+		 * "Would you like to roll again?(y/n): "
+		 * @param x
+		 * @return
+		 */
+	public static boolean askUserYN (String x) {
+		boolean flag = true;
+		System.out.println(x);
+		Scanner scnr = new Scanner(System.in);
+		String yesNo = scnr.next().trim().toLowerCase();	
+		if (yesNo.equals("n")) {
+			flag = false;
+			System.out.println("\n" + "Thank you for visiting the Grand Circus Casino.");
+		}else if(yesNo.equals("y")){
+			 flag = true;
+		}
+		return flag;
+		}
 
 	}
-}
