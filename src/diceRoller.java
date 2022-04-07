a86094ac41700cfc592c39a94a91cf17d03703ae
Bolanle <bola0321@gmail.com>
2022-04-06 21:39:12 -0400

    added dice roller method

--- src/diceRoller.java
+++ src/diceRoller.java
@@ -7,45 +7,54 @@ public class diceRoller {
 		// prompt user to roll dice
 		// roll two dice of user selection size
 		// ask user to if the want to roll the dice again
-		
-		//TODO: insert roll counter logic (roll 1, roll2, roll 3, etc....)
-		//TODO: Write generateRandomDieRoll method
+
 		int numSides;
-		int generateRandomDieRoll;
 		int randomRollOne;
 		int randomRollTwo;
 		int rollCounter;
-		
+
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Welcome to the Grand Circus Casino!");
 		System.out.print("\n" + "How many sides should each die have?");
 		numSides = scnr.nextInt();
-		
-		randomRollOne = ((int) (Math.random()*numSides) );
-		randomRollTwo = ((int) (Math.random()*numSides) );
+
+		randomRollOne = generateRandomDice(numSides);
+		randomRollTwo = generateRandomDice(numSides);
+
 		System.out.println(randomRollOne + 1);
 		System.out.println(randomRollTwo + 1);
-		
+
 		int dieSum = randomRollOne + randomRollTwo;
-		
-		if(dieSum == 2 || dieSum == 3 || dieSum == 12) {
+
+		if (dieSum == 2 || dieSum == 3 || dieSum == 12) {
 			System.out.println("Craps!");
-		}else if (randomRollOne == 0 && randomRollTwo == 0) {
+		} else if (randomRollOne == 0 && randomRollTwo == 0) {
 			System.out.println("Snake Eyes!");
-		}else if (randomRollOne == 5 && randomRollTwo == 5) {
+		} else if (randomRollOne == 5 && randomRollTwo == 5) {
 			System.out.println("Boxcars!");
 		}
-		
-		// TODO: Roll n counter
+
+// TODO: insert roll counter logic (roll 1, roll2, roll 3, etc....)
+		scnr.close();
+
 	}
 
-//	public static int randomDice(int numSides) {
-//		
+	/********************* generateRandomDice ********************** 
+	 * passes in int and randomly chooses number, returns an int
+	 */
+
+	public static int generateRandomDice(int numSides) {
+
+		int randomRoll = ((int) (Math.random() * numSides));
+
+		return randomRoll;
+
 //	}
 
 //****************** generateRandomDieRoll **************	
-	// public static void generateRandomDieRoll() {
+		// public static void generateRandomDieRoll() {
 
-	// }
+		// }
 
+	}
 }
